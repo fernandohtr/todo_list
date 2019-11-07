131b846a04b7a6d4a7ce0d2dc478949283e677f0
Fernando Henrique <fernandohtr@gmail.com>
2019-11-07 19:16:06 -0300

    Adiciona método POST de botão de submit

--- config/utils.py
+++ config/utils.py
@@ -2,7 +2,7 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def render(page, **items):
-    file_loader = FileSystemLoader('templates')
+    file_loader = FileSystemLoader('static')
     environment = Environment(loader=file_loader)
     template = environment.get_template(page)
     return template.render(**items)
--- todo_list.py
+++ todo_list.py
@@ -1,5 +1,8 @@
 import falcon
 import os
+import json
+
+from urllib.parse import parse_qs
 
 from config.utils import render
 
@@ -9,11 +12,23 @@ class TodoResource(object):
     def on_get(self, request, response):
         response.status = falcon.HTTP_200
         response.content_type = 'text/html; charset=utf-8'
-        response.body = render('main.html', list_items=self.list_items())
+        content = {"items": ["Estudar", "Comprar pão"]}
+        response.body = render('main.html', list_items=self._list_items())
 
-    def list_items(self):
+    def on_post(self, request, response):
+        data_serialize = request.stream.read()
+        data_byte = parse_qs(data_serialize)[b'task'][0]
+        data = data_byte.decode('utf-8')
+        self._save_info(data)
+        self.on_get(request, response)
+
+    def _list_items(self):
         with open(os.path.abspath('config/save_list.txt')) as file:
             return file.readlines()
+
+    def _save_info(self, read_str):
+        with open(os.path.abspath('config/save_list.txt'), 'a') as file:
+            return file.write(read_str + '\n')
 
 
 app = falcon.API()
